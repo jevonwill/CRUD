164317710b176d6585a80175d30cea40efc045b4
jevonwill <jevonwilliamsjr@gmail.com>
2022-12-02 12:13:54 -0500

    Created inital react pages

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import {toast} from 'react-toastify'
+
+
+const home = () => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
